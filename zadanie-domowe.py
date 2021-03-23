ce05878279afdaeddd8e7b03564b0fe188995a6c
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-03-23 11:35:27 +0100

    does not work properly I did it wrong, I'm looking for a new way I'm looking for a new pattern

--- zadanie-domowe.py
+++ zadanie-domowe.py
@@ -30,35 +30,31 @@ c_x = int(input('podaj punkt na osi x trzeciego wierzcholka'))
 c_y = int(input('podaj punkt na osi y trzeciego wierzcholka '))
 print('\n')
 
-# wzor na pole trojkata przy znanych wierzcholkach
-# https://szaloneliczby.pl/pole-trojkata-abc-o-wierzcholkach-a-0-0-b-4-2-c-2-6-jest-rowne/
-# p1 = (1/2) * Xa * Ya
-# p2 = (1/2) * Xb * Yb
-# p3 = (1/2) * Xc * Yc
-# wyliczam pola trójkątów dookoła trójkąta głównego
-triangle_area_1 = (1/2) * a_x * c_y
-print(f'pole trojkata 1 to {triangle_area_1}')
-triangle_area_2 = (1/2) * a_y * b_x
-print(f'pole trojkata 2 to {triangle_area_2}')
-triangle_area_3 = (1/2) * c_x * b_y
-print(f'pole trojkata 3 to {triangle_area_3}')
-# pole prostokatu, w ktorym jest trojkat
-# rectangle_area =
-rectangle_area = (a_x + c_y) * (a_y + b_x)
-
-print(f'pole prostokatu to {rectangle_area}')
-
-
-# suma pól trójkątów bocznych
-# triangle_area_123 = p1 + p2 + p3
-triangle_area_123 = triangle_area_1 + triangle_area_2 + triangle_area_3
-
-print(f'suma pol wszystkich trojkatow to {triangle_area_123}')
-# pole trójkąta właściwego
-# p(abc) = rectangle_area - triangle_area_123
-main_triangle_area = rectangle_area - triangle_area_123
-
-print(f"Pole trójkąta o wierzchołkach ({a_x} , {a_y}), ({b_x} , {b_y}), ({c_x} , {c_y}) wynosi {main_triangle_area}")
+# # wzor na pole trojkata przy znanych wierzcholkach
+# # https://szaloneliczby.pl/pole-trojkata-abc-o-wierzcholkach-a-0-0-b-4-2-c-2-6-jest-rowne/
+# # p1 = (1/2) * (Ya + Yc) * (Ya + Xc)
+# # p2 = (1/2) * (Xa + Xb) * (Xb + Yb)
+# # p3 = (1/2) * ((a_y + c_y) - (a_y + b_y)) * ((a_x + b_x) - (a_y + c_x))
+# # wyliczam pola trójkątów dookoła trójkąta głównego
+# triangle_area_1 = (1/2) * (a_y + c_y) * (a_y + c_x)
+# print(f'pole trojkata 1 to {triangle_area_1}')
+# triangle_area_2 = (1/2) * (a_x + b_x) * (a_y + b_y)
+# print(f'pole trojkata 2 to {triangle_area_2}')
+# triangle_area_3 = (1/2) * ((a_y + c_y) - (a_y + b_y)) * ((a_x + b_x) - (a_y + c_x))
+# # print((b_y + c_y) - (a_x + b_y))
+# print(f'pole trojkata 3 to {triangle_area_3}')
+# # pole prostokatu, w ktorym jest trojkat
+# # rectangle_area = (a_x + c_y) * (a_y + b_x)
+# rectangle_area = (a_x + c_y) * (a_y + b_x)
+# print(f'pole prostokatu to {rectangle_area}')
+# # suma pól trójkątów bocznych
+# # triangle_area_123 = p1 + p2 + p3
+# triangle_area_123 = triangle_area_1 + triangle_area_2 + triangle_area_3
+# print(f'suma pol wszystkich trojkatow to {triangle_area_123}')
+# # pole trójkąta właściwego
+# # p(abc) = rectangle_area - triangle_area_123
+# main_triangle_area = rectangle_area - triangle_area_123
+# print(f"Pole trójkąta o wierzchołkach ({a_x} , {a_y}), ({b_x} , {b_y}), ({c_x} , {c_y}) wynosi {main_triangle_area}")
 
 # ****************************************************************************************************************
 # ****************************************************************************************************************
