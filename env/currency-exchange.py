6cfade2a8673dc680152b703b92429041c958d29
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-03-26 13:53:31 +0100

    implement switch case statement

--- env/currency-exchange.py
+++ env/currency-exchange.py
@@ -1,5 +1,3 @@
-
-
 # Prepare two variables, containing the buying rate and the USD selling rate. As of today,
 # this is the purchase of 3.8507, the sale of 3.9285.
 # Your task will be to ask the user what he wants to buy / sell, then ask him for
@@ -12,4 +10,30 @@
 # następnie zapytać go o ilość złotówek lub dolarów do wymiany,
 # a następnie poinformować jaką wartość po wymianie użytkownik otrzyma.
 
+purchase = 3.8507
+sale = 3.9285
+pln = "Pln"
+usd = " Usd"
+funds_amount_text = "enter the amount of funds to be exchanged"
+funds_amount = 0
 
+
+def usd_to_pln():
+    gained_founds = funds_amount * sale
+    return gained_founds
+
+
+def pln_to_usd():
+    gained_founds = funds_amount * purchase
+    return gained_founds
+
+
+def menu(argument):
+    switcher = {
+        "1": "Wymianę USD na Pln",
+        "2": "Wymianę Pln na USD",
+    }
+    print(f"Wybrał/a pan/i {switcher.get(argument, 'zły numer')}")
+
+
+menu(input("prosze wybrac opcje menu"))
