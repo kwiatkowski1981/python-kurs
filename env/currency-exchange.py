93e3dbd8d1dc979a821cfcbd74205275e55b69ed
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-03-26 11:59:41 +0100

    added the content of the task

---
+++ env/currency-exchange.py
@@ -0,0 +1,15 @@
+
+
+# Prepare two variables, containing the buying rate and the USD selling rate. As of today,
+# this is the purchase of 3.8507, the sale of 3.9285.
+# Your task will be to ask the user what he wants to buy / sell, then ask him for
+# the amount of zlotys or dollars to be exchanged, and then inform what value the user will receive after the exchange.
+
+
+# Przygotuj dwie zmienne, zawierające kurs kupna oraz kurs sprzedaży USD.
+# Na dzień dzisiejszy to kupno 3.8507, sprzedaż 3,9285.
+# Twoim zadaniem będzie zapytać użytkownika o to co chce zrobić kupić/sprzedać,
+# następnie zapytać go o ilość złotówek lub dolarów do wymiany,
+# a następnie poinformować jaką wartość po wymianie użytkownik otrzyma.
+
+
