1aeb52f64b6c1d3ba0fda9881807486dbb6d664e
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-03-26 09:15:31 +0100

    I provided the content of the assignment and extracted the data

---
+++ env/lib/bottling-plant.py
@@ -0,0 +1,18 @@
+
+
+# DATA
+
+# **********************************************************************************************************************
+# The boss ordered the employee to fill 330 ml bottles with liquid in such a way that 12% of the bottle was empty.
+# The employee made an agreement with the boss that he could leave all the liquid that would remain for him and would
+# not be enough to fill the bottles. After take-off, receive information from the user how many ml of fluid was supplied
+# by the factory, and the result should be the amount of liquid that the employee will be able to take with him :)
+# **********************************************************************************************************************
+
+# bottles 330ml
+# filling the bottle  - 12%
+
+# input >>>>>>>  how much liquid was delivered to the bottling plant?
+# output >>>>>>  how many bottles are needed to spill the liquid provided
+# output >>>>>>  how much fluid will be left for the employee
+
