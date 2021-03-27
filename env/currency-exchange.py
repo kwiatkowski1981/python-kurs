db1c04e0b7d2334ec2112e36896fe61819c5b9ac
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-03-27 14:00:44 +0100

    added switch case statement to my functions

--- env/currency-exchange.py
+++ env/currency-exchange.py
@@ -1,3 +1,5 @@
+import sys
+
 # Prepare two variables, containing the buying rate and the USD selling rate. As of today,
 # this is the purchase of 3.8507, the sale of 3.9285.
 # Your task will be to ask the user what he wants to buy / sell, then ask him for
@@ -13,27 +15,65 @@
 purchase = 3.8507
 sale = 3.9285
 pln = "Pln"
-usd = " Usd"
-funds_amount_text = "enter the amount of funds to be exchanged"
-funds_amount = 0
+usd = "Usd"
 
 
-def usd_to_pln():
+def ask_for_amount():
+    funds_amount = int(input("wybierz ilość środków do wymiany"))
+    # print(f"przed retun {funds_amount}")
+    return funds_amount
+
+
+def usd_to_pln(funds_amount):
     gained_founds = funds_amount * sale
-    return gained_founds
+    return print(f"{funds_amount}(USD) zamieniono na: {gained_founds}(PLN).")
 
 
-def pln_to_usd():
+def pln_to_usd(funds_amount):
     gained_founds = funds_amount * purchase
-    return gained_founds
+    return print(f"{funds_amount}(PLN) zamieniono na: {gained_founds}(USD).")
 
 
-def menu(argument):
+def menu_1():
+    print(f"Wybrano wymianę USD na PLN")
+    usd_to_pln(ask_for_amount())
+    return sys.exit(1)
+
+
+def menu_2():
+    print(f"Wybrano wymianę PLN na USD")
+    pln_to_usd(ask_for_amount())
+    return sys.exit(1)
+
+
+def menu_4():
+    return sys.exit(1)
+
+
+def error():
+    print(f"nie ma takiej opcji")
+    return sys.exit(1)
+
+
+def menu_0(argument):
     switcher = {
-        "1": "Wymianę USD na Pln",
-        "2": "Wymianę Pln na USD",
+        1: menu_1,
+        2: menu_2,
+        # 3: wroc do menu,
+        4: menu_4,
     }
-    print(f"Wybrał/a pan/i {switcher.get(argument, 'zły numer')}")
+    func = switcher.get(argument, lambda: error())
+    return func()
 
 
-menu(input("prosze wybrac opcje menu"))
+def main_menu():
+    print("Witam w kantorze")
+    print("Dostępne opcje to:")
+    print(f"1: Wymiana {usd} na {pln}")
+    print(f"2: Wymiana {pln} na {usd}")
+    print("3: Zakończ")
+    print("wybierz opcję:")
+    menu_0(int(input()))
+
+
+main_menu()
