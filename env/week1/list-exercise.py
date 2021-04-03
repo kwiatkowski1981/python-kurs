0efe4cedc820e654b80fe080f268c85758486bb4
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-04-03 14:08:37 +0200

    I added an exercise with books, in which I number the lines of displayed items, I create a new variable that records information about "id", you can start from index 1 using the second parameter after the decimal point "start = 1" when printing

--- env/week1/list-exercise.py
+++ env/week1/list-exercise.py
@@ -64,27 +64,26 @@ print(f"{names2_strings}")
 # print(names2_strings)
 
 
+num = int(input('podaj liczbe: '))
+print(num)
 
-# num = int(input('podaj liczbe: '))
-# print(num)
-#
-# if num > 1:
-#     # Iterate from 2 to n / 2
-#     for i in range(2, int(num/2)+1):
-#         # If num is divisible by any number between
-#         # 2 and n / 2, it is not prime
-#         if (num % i) == 0:
-#             print(num, "is not a prime number")
-#             break
-#     else:
-#         print(num, "is a prime number")
-#
-# else:
-#     print(num, "is not a prime number")
-#
-# print('\n')
-#
-# books = ['frunky furbo', 'magiczny kod', 'kryształy czasu']
-# for number, book in enumerate(reversed(books), start=1):        # mozna uzyc sorted
-#     print(number, book)
+if num > 1:
+    # Iterate from 2 to n / 2
+    for i in range(2, int(num/2)+1):
+        # If num is divisible by any number between
+        # 2 and n / 2, it is not prime
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+
+else:
+    print(num, "is not a prime number")
+
+print('\n')
+
+books = ['frunky furbo', 'magiczny kod', 'kryształy czasu']
+for number, book in enumerate(reversed(books), start=1):        # mozna uzyc sorted
+    print(number, book)
 
