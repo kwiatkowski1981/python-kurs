866e8d73281111dcc4431e50e68bdeae33f057be
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-04-05 18:16:43 +0200

    added homework-1, it is a string char counter

---
+++ env/week1/homework-1.py
@@ -0,0 +1,26 @@
+
+input_word = input("podaj słowo, w którym mają być policzone litery: ")
+counts = dict()
+
+for char in input_word:
+    # print(char)
+    if char in counts:
+        counts[char] += 1
+    else:
+        counts[char] = 1
+
+print(f"1) ==>  {counts}")
+print(type(counts))
+
+for key, value in counts.items():
+    print(key, ' | ', value)
+print('\n')
+
+for item in counts.items():
+    print(f"2) ==> : {item[0]} = {item[1]}")
+print(type(item))
+print('\n')
+
+for input_word, id in sorted(counts.items(), key=lambda item: item[1], reverse=True):
+    print(f'3) ==> : {input_word, id}', sep=" | ")
+print(type(input_word))
