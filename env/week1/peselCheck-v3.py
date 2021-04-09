bdec5ea0441bc91845c0f6b7a78c426424dbbb44
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-04-09 10:07:43 +0200

    added homework-4
    PESEL check v2 + v3.

--- env/week1/peselCheck-v3.py
+++ env/week1/peselCheck-v3.py
@@ -1,12 +1,15 @@
-pesel = input("podaj pesel: ")
-print(type(pesel))
-print(pesel)
-pesel_digits_sum = (int(pesel[0]) * 1) + (int(pesel[1]) * 3) + \
-                    (int(pesel[2]) * 7) + (int(pesel[3]) * 9) + \
-                    (int(pesel[4]) * 1) + (int(pesel[5]) * 3) + \
-                    (int(pesel[6]) * 7) + (int(pesel[7]) * 9) + \
-                    (int(pesel[8]) * 1) + (int(pesel[9]) * 3)
+# pesel = input("podaj pesel: ")
 
+pesel = str("81042108198")
+
+
+pesel = input("podaj pesel: ")
+pesel_pattern_digits = ['1', '3', '7', '9', '1', '3', '7', '9', '1', '3']
+pesel_digits_to_multiply = list(zip(list(pesel), pesel_pattern_digits))
+multiplied_pesel_digits = []
+for pesel_number, pattern_digit in pesel_digits_to_multiply:
+    multiplied_pesel_digits.append(int(pesel_number) * int(pattern_digit))
+pesel_digits_sum = sum(multiplied_pesel_digits)
 pesel_digits_sum_as_string = str(pesel_digits_sum)
 last_sum_digit = pesel_digits_sum_as_string[-1:]
 check_number = 10 - int(last_sum_digit[-1:])
@@ -16,5 +19,7 @@ if pesel[-1:] == check_number_as_str:
 else:
     print(f"Twój pesel: {pesel} jest niepoprawny")
 
-quit()
-# 81042108198
+
+# 81042108198       pesel
+# 1379137913        mnożnik
+# 8303623072127     wynik
--- env/week1/pesel_check.py
+++ env/week1/pesel_check.py
@@ -1,53 +1,9 @@
-pesel = input("podaj pesel: ")
-print(type(pesel))
-print(pesel)
-
-
-def convert_pesel_to_list(pesel):
-    pesel_as_list = list(pesel)
-    print(pesel_as_list)
-    return pesel_as_list
-
-
-def pesel_pattern_sum_digits(pesel_as_list):
-    pesel_digits_sum = (int(pesel_as_list[0]) * 1) + (int(pesel_as_list[1]) * 3) + \
-                        (int(pesel_as_list[2]) * 7) + (int(pesel_as_list[3]) * 9) + \
-                        (int(pesel_as_list[4]) * 1) + (int(pesel_as_list[5]) * 3) + \
-                        (int(pesel_as_list[6]) * 7) + (int(pesel_as_list[7]) * 9) + \
-                        (int(pesel_as_list[8]) * 1) + (int(pesel_as_list[9]) * 3)
-    print(pesel_digits_sum)
-    return pesel_digits_sum
-
-
-def pesel_pattern_sum_digits_to_str(pesel_digits_sum):
-    pesel_digits_sum_as_string = str(pesel_digits_sum)
-    print(pesel_digits_sum_as_string)
-    return pesel_digits_sum_as_string
-
-
-def pesel_pattern_sum_last_digit(pesel_digits_sum_as_string):
-    last_sum_digit = pesel_digits_sum_as_string[-1:]
-    print(last_sum_digit)
-    return last_sum_digit
-
-
-def pesel_check_number(last_sum_digit):
-    check_number = 10 - int(last_sum_digit[-1:])
-    print(check_number)
-    return check_number
-
-
-def pesel_check_number_to_str(check_number):
-    check_number_as_str = str(check_number)
-    print(check_number_as_str)
-    return check_number_as_str
-
-
-def check_if_last_digit_of_pesel_is_equal_to_check_number_as_str( check_number_as_str):
-    if pesel[-1:] == check_number_as_str:
-        print(f"Twój pesel: {pesel} jest poprawny")
-    else:
-        print(f"Twój pesel: {pesel} jest niepoprawny")
-
-
+pesel = input("Podaj pesel: ")
+pesel_list = list(pesel)
+list_q = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1]
+suma = 0
+for pes, quan in zip(pesel, list_q):
+    suma += int(pes) * quan
+print(suma)
+print(f"Pesel {pesel} jest poprawny") if suma % 10 == 0 else print(f"Pesel {pesel} nie jest prawidłowy")
 # 81042108198
