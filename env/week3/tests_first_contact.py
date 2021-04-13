97728f958ff941dade08834c1e7e3554fe45c3f9
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-04-13 22:21:30 +0200

    added simple count_letters between brackets program tests

--- env/week3/tests_first_contact.py
+++ env/week3/tests_first_contact.py
@@ -10,3 +10,43 @@ def test_add_numbers():
     value = add_numbers(a, b)
     # then
     assert value == 5
+
+
+def count_letters(text, start='(', end=')'):
+    should_count = False
+    counter = 0
+    temp_counter = 0
+    for char in text:
+        if char == end:
+            should_count = False
+            counter += temp_counter
+            temp_counter = 0
+
+        if should_count:
+            temp_counter += 1
+
+        if char == start:
+            should_count = True
+
+    return counter
+
+
+def test_barteks_idea():
+    assert count_letters('Samuraj (Programowania') == 0
+
+
+def test_count_letters_once():
+    assert count_letters('Samuraj (Programowania)') == 13
+
+
+def test_count_letters_none():
+    assert count_letters('Samuraj') == 0
+
+
+def test_count_letters_override():
+    assert count_letters('Python', '>', '<') == 0
+
+
+def test_multiple_brackets():
+    assert count_letters('(Python) jest (super) silnym (językiem) programowania(!)(!)') == 21
+    assert count_letters('<Python> jest <super> silnym <językiem> programowania<!>', '<', '>') == 20
